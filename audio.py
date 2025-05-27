62a0741cb0264240316e497fd5061ff0a65c8f98
Guilherme Arcencio <guiarcencio@gmail.com>
2025-05-27 12:32:31 -0300

    Revert "try jitter buffer"
    This reverts commit 9e61d63246223cb0c365579332493d8aa95cc360.

--- audio.py
+++ audio.py
@@ -3,64 +3,38 @@ import zlib
 from collections import deque
 from queue import Empty, Queue
 from threading import Event, Thread
-from time import sleep, time
 
 import pyaudio
 import zmq
 
 SAMPLE_RATE = 16000
-BUFFER_DELAY = 50.0 / 1000.0  # 50ms
 
 
 class AudioStream:
     _stream: pyaudio.Stream
-    _writing_thread: Thread
-    _jitter_thread: Thread
-    _write_queue: Queue
-    _jitter_buffer: Queue
-    _callback_queue: Queue
-    _last_frame: int
+    _thread: Thread
+    _buffer: Queue
     _shutdown: Event
 
     def __init__(self, p: pyaudio.PyAudio = None):
         if not p:
             p = pyaudio.PyAudio()
 
-        self._write_queue = Queue()
-        self._jitter_buffer = Queue()
-        self._callback_queue = Queue()
+        self._buffer = Queue()
         self._shutdown = Event()
-
-        self._last_frame = 0
-
-        def write_callback(in_data, frame_count, time_info, status):
-            out_data = b""
-            for _ in range(frame_count):
-                try:
-                    frame = self._callback_queue.get_nowait()
-                    self._last_frame = frame
-                except Empty:
-                    frame = self._last_frame
-
-                out_data += struct.pack("=h", frame)
-
-            return (out_data, pyaudio.paContinue)
 
         self._stream = p.open(
             rate=SAMPLE_RATE,
             channels=1,
-            stream_callback=write_callback,
             format=pyaudio.paInt16,
             output=True,
         )
 
-        self._writing_thread = Thread(target=self._write_audio_data)
-        self._writing_thread.start()
-        self._jitter_thread = Thread(target=self._jitter_buffer_delay)
-        self._jitter_thread.start()
+        self._thread = Thread(target=self._write_audio_data)
+        self._thread.start()
 
     def write(self, data: bytes):
-        self._write_queue.put(data)
+        self._buffer.put(data)
 
     def stop(self):
         self._shutdown.set()
@@ -69,26 +43,20 @@ class AudioStream:
     def _write_audio_data(self):
         while not self._shutdown.is_set():
             try:
-                data = self._write_queue.get(timeout=0.5)
+                data = self._buffer.get(timeout=0.5)
             except Empty:
                 continue
             data = zlib.decompress(data)
 
+            native_data = b""
             for (frame,) in struct.iter_unpack("!h", data):
-                self._jitter_buffer.put((frame, time()))
+                native_data += struct.pack("=h", frame)
 
-    def _jitter_buffer_delay(self):
-        while not self._shutdown.is_set():
             try:
-                frame, arrival = self._jitter_buffer.get(timeout=0.5)
-            except Empty:
-                continue
-
-            current_time = time()
-            if current_time - arrival < BUFFER_DELAY:
-                sleep(BUFFER_DELAY - (current_time - arrival))
-
-            self._callback_queue.put(frame)
+                self._stream.write(native_data)
+            except OSError:
+                # Stream closed
+                return
 
 
 class AudioManager:
